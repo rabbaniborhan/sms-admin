fc8fefc1f93d87a059d369a2a9f9a2d730206b20
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-09-14 20:33:20 +0600

    Fix running notice padding issue --> Done

--- src/pages/notice/notice-board/RunningNotice/RunningNotice.jsx
+++ src/pages/notice/notice-board/RunningNotice/RunningNotice.jsx
@@ -4,7 +4,7 @@ import { Edit, Delete } from "../../../../constants/icons";
 const RunningNotice = ({ setShowModal }) => {
   return (
     <div className='w-full flex justify-center items-center z-30'>
-      <div className='w-[10%] bg-yellow py-4 text-center font-semibold text-white ring-1 ring-white'>
+      <div className='w-[10%] bg-yellow py-[17px] text-center font-semibold text-white ring-1 ring-white'>
         <p className='font-sans'>Latest News</p>
       </div>
       <div className='w-11/12 py-4 text-center bg-[#1EB3A6]/50 flex justify-between items-center font-bangla'>
