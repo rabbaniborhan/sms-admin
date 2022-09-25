5671a46136f8eec5f92d7b08d28f8433b9a40ba0
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-09-25 22:16:57 +0600

    Create Dashboard total and Chart section --> Done

---
+++ src/components/DashboardParts/DashboardTotalSection.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import images from "../../assets";
+import Image from "next/image";
+
+const DashboardTotalSection = () => {
+  return (
+    <div className='w-full mx-auto flex justify-between items-center'>
+      <div className='w-80 relative overflow-hidden flex justify-start items-start gap-5 p-3 bg-#fff shadow-md rounded-md ring-1 ring-[#0D153066]'>
+        <span className='absolute -right-2 -top-3'>
+          <Image src={images.yellowCircle} alt='yellow_circle' />
+        </span>
+        <span className='w-16 h-16 rounded-full bg-[#BCF1ED99] flex justify-center items-center'>
+          <Image src={images.studentPng} width={45} height={45} />
+        </span>
+        <div className='flex flex-col justify-start items-start gap-y-1'>
+          <h4 className='text-3xl font-semibold text-[#1EB3A6]'>3500+</h4>
+          <p className='text-xl font-semibold'>Total Student</p>
+          <div className='flex justify-between items-center gap-4'>
+            <span className='flex justify-between items-center gap-2'>
+              <p className='font-semibold text-[#1EB3A6]'>2000+</p>
+              <p className='font-[500]'>Boys</p>
+            </span>
+            <span className='flex justify-between items-center gap-2'>
+              <p className='font-semibold text-[#FF9900]'>1500+</p>
+              <p className='font-[500]'>Girls</p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-80 relative overflow-hidden flex justify-start items-start gap-5 p-3 bg-#fff shadow-md rounded-md ring-1 ring-[#0D153066]'>
+        <span className='absolute -right-2 -top-3'>
+          <Image src={images.yellowCircle} alt='yellow_circle' />
+        </span>
+        <span className='w-16 h-16 rounded-full bg-[#BCF1ED99] flex justify-center items-center'>
+          <Image src={images.studentPng} width={45} height={45} />
+        </span>
+        <div className='flex flex-col justify-start items-start gap-y-1'>
+          <h4 className='text-3xl font-semibold text-[#1EB3A6]'>50+</h4>
+          <p className='text-xl font-semibold'>Teacher</p>
+          <div className='flex justify-between items-center gap-4'>
+            <span className='flex justify-between items-center gap-2'>
+              <p className='font-semibold text-[#1EB3A6]'>30+</p>
+              <p className='font-[500]'>Male</p>
+            </span>
+            <span className='flex justify-between items-center gap-2'>
+              <p className='font-semibold text-[#FF9900]'>20+</p>
+              <p className='font-[500]'>Female</p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-80 relative overflow-hidden flex justify-start items-start gap-5 p-3 bg-#fff shadow-md rounded-md ring-1 ring-[#0D153066]'>
+        <span className='absolute -right-2 -top-3'>
+          <Image src={images.yellowCircle} alt='yellow_circle' />
+        </span>
+        <span className='w-16 h-16 rounded-full bg-[#BCF1ED99] flex justify-center items-center'>
+          <Image src={images.moneyPng} width={45} height={45} />
+        </span>
+        <div className='flex flex-col justify-start items-start gap-y-1'>
+          <h4 className='text-3xl font-semibold text-[#1EB3A6]'>৳ 20,1570+</h4>
+          <p className='text-xl font-semibold'>Collection</p>
+          <div className='flex justify-between items-center gap-4'>
+            <span className='flex justify-between items-center gap-2'>
+              <p className='font-semibold text-[#1EB3A6]'>200+</p>
+              <p className='font-[500]'>Fees</p>
+            </span>
+            <span className='flex justify-between items-center gap-2'>
+              <p className='font-semibold text-[#FF9900]'>300+</p>
+              <p className='font-[500]'>Fund</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTotalSection;
---
+++ src/components/DashboardParts/LineChart.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const colorItems = (tooltipItem) => {
+  const tooltipBg = tooltipItem.tooltip.labelColors[0].borderColor;
+  return tooltipBg;
+};
+
+export const options = {
+  responsive: true,
+  plugins: {
+    tooltip: {
+      displayColors: false,
+      backgroundColor: colorItems,
+    },
+    legend: {
+      display: false,
+      position: "top",
+      align: "start",
+    },
+  },
+};
+
+const labels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const arr1 = [
+  30000, 20000, 40000, 60000, 50000, 70000, 90000, 80000, 100000, 120000,
+  110000, 100000,
+];
+const arr2 = [
+  20000, 10000, 30000, 50000, 40000, 60000, 80000, 70000, 90000, 110000, 100000,
+  120000,
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Fees Collection",
+      data: arr1,
+      borderColor: "#FF9900",
+      backgroundColor: "#FF9900",
+      tension: 0.5,
+    },
+    {
+      label: "Total Collection",
+      data: arr2,
+      borderColor: "#1EB3A6",
+      backgroundColor: "#1EB3A6",
+      tension: 0.5,
+    },
+  ],
+};
+
+const LineChart = () => {
+  return (
+    <div className='w-3/5 bg-white border mt-10 border-gray-300 py-5 px-5 rounded-md'>
+      <div>
+        <div>
+          <h3 className='font-semibold text-xl'>Earnings</h3>
+          <div className='flex justify-start gap-5 my-3'>
+            <span className='flex justify-center items-center gap-3'>
+              <p className='w-2 h-2 rounded-full bg-[#1EB3A6]'></p>
+              <span>
+                <p className='text-[#1EB3A6] text-md font-semibold'>200k+</p>
+                <p className='text-xs'>Total Collection</p>
+              </span>
+            </span>
+
+            <span className='flex justify-center items-center gap-3'>
+              <p className='w-2 h-2 rounded-full bg-[#FF9900]'></p>
+              <span>
+                <p className='text-[#FF9900] text-md font-semibold'>1500k+</p>
+                <p className='text-xs'>Fees Collection</p>
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default LineChart;
