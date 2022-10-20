24632ff190adf1df1e0a2a5682dc663b138c062a
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-10-20 10:49:26 +0600

    Fix mark distribution issue --> Done

--- src/pages/exams/mark-distribution/mark-sheet-list/MarkSheetListTable/MarkSheetListTableData.jsx
+++ src/pages/exams/mark-distribution/mark-sheet-list/MarkSheetListTable/MarkSheetListTableData.jsx
@@ -15,12 +15,14 @@ const MarkSheetListTableData = ({ reportData }) => {
         {reportData?.subject}
       </td>
       <td className='border-[0.5px] border-gray-500 group'>
-        <input type='text' className='w-full py-4 text-center' />
+        <input type='text' className='w-full py-4 text-center outline-none' />
       </td>
       <td className='border-[0.5px] border-gray-500 group'>
-        <input type='text' className='w-full py-4 text-center' />
+        <input type='text' className='w-full py-4 text-center outline-none' />
       </td>
-      <td className='border-[0.5px] border-gray-500 py-4'></td>
+      <td className='border-[0.5px] border-gray-500'>
+        <input type='text' className='w-full py-4 text-center outline-none' />
+      </td>
       <td colSpan={3} className='border-[0.5px] border-gray-500 py-4'>
         {reportData?.total}
       </td>
