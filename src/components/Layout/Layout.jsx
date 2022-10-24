b8e254676cfb47bccdec07e1b2ffcf4dbcef008f
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-10-24 17:17:02 +0600

    Add loading component --> Done

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,11 @@
-import Cookies from "js-cookie";
-import { NextShield } from "next-shield";
 import Head from "next/head";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 
-const Layout = ({ children, router }) => {
+const Layout = ({ children }) => {
   const show = useSelector((state) => state.ui.showLayout);
-  const token = Cookies.get("jwtToken");
   return (
     <div>
       <Head>
@@ -16,29 +13,17 @@ const Layout = ({ children, router }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <NextShield
-        isAuth={token}
-        isLoading={false}
-        router={router}
-        privateRoutes={["/", "/admission"]}
-        publicRoutes={["/login"]}
-        accessRoute='/'
-        loginRoute='/login'
-        LoadingComponent={<p>Loading...</p>}>
-        {show ? (
-          <main className='relative'>
-            <Navbar />
-            <div className='flex justify-center items-center'>
-              <Sidebar />
-              <div className='w-4/5 ml-52 overflow-hidden mt-20'>
-                {children}
-              </div>
-            </div>
-          </main>
-        ) : (
-          children
-        )}
-      </NextShield>
+      {show ? (
+        <main className='relative'>
+          <Navbar />
+          <div className='flex justify-center items-center'>
+            <Sidebar />
+            <div className='w-4/5 ml-52 overflow-hidden mt-20'>{children}</div>
+          </div>
+        </main>
+      ) : (
+        children
+      )}
     </div>
   );
 };
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Grid } from "react-loader-spinner";
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div className='fixed inset-0 z-30 flex justify-center items-center'>
+      <Grid
+        height='80'
+        width='80'
+        color='#1eb3a6'
+        ariaLabel='grid-loading'
+        radius='12.5'
+        wrapperStyle={{}}
+        wrapperClass='m-auto'
+        visible={true}
+      />
     </div>
   );
 };
